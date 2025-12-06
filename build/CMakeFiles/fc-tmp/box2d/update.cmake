d4531e42101e15205fc93a6740279222b898e2b5
Will The Water <168959918+WillTheWater@users.noreply.github.com>
2025-12-06 20:20:12 +0100

    Removed redundant build directory

--- build/CMakeFiles/fc-tmp/box2d/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.29)
-
-message(VERBOSE "Executing update step for box2d")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Dev/Chess/build/CMakeFiles/fc-tmp/box2d/box2d-gitupdate.cmake")
-
-endblock()
