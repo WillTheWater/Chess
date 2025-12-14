ea338b929bb2222ceeffa5fa6fa24cde6774e826
WillTheWater <willthewater@protonmail.com>
2025-12-14 12:03:29 +0100

    Refactored Asset Manager

--- WaterEngine/include/Framework/AssetManager.h
+++ WaterEngine/include/Framework/AssetManager.h
@@ -17,9 +17,50 @@ namespace we
 		AssetManager();
 
 	private:
+		template<typename Asset, typename MapType, typename LoadFunc>
+		shared<Asset> LoadAsset(const string& Path, MapType& Cache, LoadFunc&& Loader);
+
+
+		template<typename MapType>
+		void GarbageCollect(MapType& Cache);
+
 		static unique<AssetManager> UAssetManager;
 		Dictionary<string, shared<sf::Texture>> LoadedTextures;
 		Dictionary<string, shared<sf::Font>> LoadedFonts;
 		string RootDirectory;
 	};
+
+	template<typename Asset, typename MapType, typename LoadFunc>
+	inline shared<Asset> AssetManager::LoadAsset(const string& Path, MapType& Cache, LoadFunc&& Loader)
+	{
+		auto it = Cache.find(Path);
+		if (it != Cache.end())
+			return it->second;
+
+		auto asset = std::make_shared<Asset>();
+
+		if (Loader(*asset, RootDirectory + Path))
+		{
+			Cache.emplace(Path, asset);
+			return asset;
+		}
+
+		return nullptr;
+	}
+
+	template<typename MapType>
+	inline void AssetManager::GarbageCollect(MapType& Cache)
+	{
+		for (auto it = Cache.begin(); it != Cache.end();)
+		{
+			if (it->second.unique())
+			{
+				it = Cache.erase(it);
+			}
+			else
+			{
+				++it;
+			}
+		}
+	}
 }
--- WaterEngine/src/Framework/AssetManager.cpp
+++ WaterEngine/src/Framework/AssetManager.cpp
@@ -15,61 +15,28 @@ namespace we
 
 	shared<sf::Texture> AssetManager::LoadTexture(const std::string& TexturePath)
 	{
-		auto FoundTexture = LoadedTextures.find(TexturePath);
-		if (FoundTexture != LoadedTextures.end())
-		{
-			return FoundTexture->second;
-		}
-		shared<sf::Texture> NewTexture{ new sf::Texture };
-		if (NewTexture->loadFromFile(RootDirectory + TexturePath))
-		{
-			LoadedTextures.insert({ TexturePath, NewTexture });
-			return NewTexture;
-		}
-		return shared<sf::Texture> {nullptr};
+		return LoadAsset<sf::Texture>(TexturePath, LoadedTextures,
+			[](sf::Texture& tex, const string& path)
+			{
+				return tex.loadFromFile(path);
+			}
+		);
 	}
 
 	shared<sf::Font> AssetManager::LoadFont(const string& FontPath)
 	{
-		auto FoundFont = LoadedFonts.find(FontPath);
-		if (FoundFont != LoadedFonts.end())
-		{
-			return FoundFont->second;
-		}
-		shared<sf::Font> NewFont{ new sf::Font };
-		if (NewFont->openFromFile(RootDirectory + FontPath))
-		{
-			LoadedFonts.insert({ FontPath, NewFont });
-			return NewFont;
-		}
-		return shared<sf::Font> {nullptr};
+		return LoadAsset<sf::Font>(FontPath, LoadedFonts,
+			[](sf::Font& font, const string& path)
+			{
+				return font.openFromFile(path);
+			}
+		);
 	}
 
 	void AssetManager::GarbageCollectionCycle()
 	{
-		for (auto i = LoadedTextures.begin(); i != LoadedTextures.end();)
-		{
-			if (i->second.unique())
-			{
-				i = LoadedTextures.erase(i);
-			}
-			else
-			{
-				i++;
-			}
-		}
-
-		for (auto i = LoadedFonts.begin(); i != LoadedFonts.end();)
-		{
-			if (i->second.unique())
-			{
-				i = LoadedFonts.erase(i);
-			}
-			else
-			{
-				i++;
-			}
-		}
+		GarbageCollect(LoadedTextures);
+		GarbageCollect(LoadedFonts);
 	}
 
 	void AssetManager::SetAssetRootDirctory(const std::string& Directory)
